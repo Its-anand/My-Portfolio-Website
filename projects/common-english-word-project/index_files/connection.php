9d99fff92c97594d4878948ec41298baeb02883c
anand <do4connect@gmail.com>
2023-09-21 20:06:15 +0530

    database

--- projects/common-english-word-project/index_files/connection.php
+++ projects/common-english-word-project/index_files/connection.php
@@ -1,12 +1,9 @@
 <?php
 $server = "localhost";
-$username = "root";
-$password = "";
+$username = "anandcho_anand";
+$password = "1qtH^#i?bp[a";
 $db = "anandcho_databases";
 
-// $username = "anandcho_anandy";
-// $password = "1qtH^#i?bp[a";
-// $db = "id19096300_account";
 $con = new mysqli($server, $username, $password, $db);
 mysqli_set_charset($con, "utf8");
 /*if ($con->connect_error) {
