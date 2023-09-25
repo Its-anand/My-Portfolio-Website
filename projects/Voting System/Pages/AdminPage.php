3ac7dfd74b924c6bc9c301e6017b4ba70b29d671
anand <do4connect@gmail.com>
2023-09-25 17:28:08 +0530

    upgrading voting system

--- projects/Voting System/Pages/AdminPage.php
+++ projects/Voting System/Pages/AdminPage.php
@@ -8,7 +8,8 @@ if(!isset($_SESSION['AdminLoginId']))
     header("location: adminLogin.html");
 }
 ?>
-<head>
+
+ <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,12 +17,13 @@ if(!isset($_SESSION['AdminLoginId']))
     <style>
         body {
             margin: 0;
-            height:100vh;
-            background: #0D47A1;
+            height: 100vh;
+            background: linear-gradient(#29317b, #1f2060, #161045);
             padding: 0%;
-            display:flex;
-            justify-content:center;
-            align-items:center;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-family: sans-serif;
         }
 
         .control_panel {
@@ -34,7 +36,9 @@ if(!isset($_SESSION['AdminLoginId']))
             box-sizing: border-box;
 
         }
+
         .form_holder form {
+            
             text-align: center;
         }
 
@@ -56,9 +60,6 @@ if(!isset($_SESSION['AdminLoginId']))
             height: 30px;
         }
 
-        #submit_btn:hover {
-            background-color: #0d48a158;
-        }
 
         #submit_btn {
             height: 35px;
@@ -67,13 +68,19 @@ if(!isset($_SESSION['AdminLoginId']))
             border: none;
             font-size: 1.2rem;
             cursor: pointer;
+            transition: 0.3s;
+            border-radius: 8px;
+        }
+
+        #submit_btn:hover {
+            border-radius: 15px;
 
         }
-        .heading
-         {
-           font-family: sans-serif; 
-           color: #0D47A1;
-         }
+        .heading {
+            font-family: sans-serif;
+            color: #0D47A1;
+        }
+
         #signIn,
         #forget_btn {
             text-decoration: none;
@@ -83,55 +90,66 @@ if(!isset($_SESSION['AdminLoginId']))
             font-family: sans-serif;
         }
 
-        .image_loader{
+        .image_student_loader {
             color: #0D47A1;
             border: none;
             font-size: 1rem;
             cursor: pointer;
-            width:100%;
+            width: 100%;
 
-    }
-    .form_text
-    {
-        width:100%;
-        height:250px;
-        resize: vertical;
-        border: none;
-    border-bottom: 1px solid #0D47A1;
-    background: #0d48a158;
-    margin-bottom: 2rem;
-    padding: 10px;
-    box-sizing: border-box;
-}
-    
+        }
+        .party_logo{
+            margin: 0%;
+        }
+        .form_text {
+            width: 100%;
+            height: 250px;
+            resize: vertical;
+            border: none;
+            border-bottom: 1px solid #0D47A1;
+            background: #0d48a158;
+            margin-bottom: 2rem;
+            padding: 10px;
+            box-sizing: border-box;
+        }
+
         @media screen and (max-width:475px) {
             body {
-            background: #fff;
-        }
-        .control_panel{
-            width: 100%;
-            padding: 2rem;
-            height: unset;
-        }
+                background: #fff;
+            }
 
-    }
+            .control_panel {
+                width: 100%;
+                padding: 2rem;
+                height: unset;
+            }
+
+        }
     </style>
 </head>
-        <div class="control_panel">
-            <div class="form_holder">
+
+<body>
+    <div class="control_panel">
+        <div class="form_holder">
             <h2 class='heading'>Add party</h2>
-                <form method="post" action="../Database/Add Party.php" enctype="multipart/form-data"> <!--enctype="multipart/form-data" is necessary if the admin will upload a file through the form here file is image. enctype only work with method='post' -->          
-                <input type="file" name="party_logo" class="image_loader">
+            <form method="post" action="../Database/Add Party.php" enctype="multipart/form-data">
+                <!--enctype="multipart/form-data" is necessary if the admin will upload a file through the form here file is image. enctype only work with method='post' -->
+                <div style="text-align: left; ">
+                    <p for="party_logo">Party Logo</p>
+                    <input type="file" name="party_logo" class="image_student_loader party_logo">
+                    <p for="">Student Data</p>
+                    <input type="file" name="voters_id" class="image_student_loader">
+                </div>
                 <input type="text" name="party_name" placeholder="Party Name *" class="form_input">
                 <input type="text" name="candidate_name" placeholder="Candidate Name *" class="form_input">
-                <input type="submit" value="Add party" name="submit" id="submit_btn"  class="form_btn" >
+                <input type="submit" value="Add party" name="submit" id="submit_btn" class="form_btn">
                 <a class="form_btn" href="studentLogin.html" id="signIn">Go Back</a>
-                </form>
-            </div>
+            </form>
         </div>
     </div>
+    </div>
 
-</body>
+</body> 
 
 
 </html>
