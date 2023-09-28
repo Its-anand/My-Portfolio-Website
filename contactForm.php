8f819c5ddade7c394268e4bd1e7de2709f511eda
anand <do4connect@gmail.com>
2023-09-28 12:37:45 +0530

    contact form changed

--- contactForm.php
+++ contactForm.php
@@ -6,16 +6,13 @@ if(isset($_POST['Submit']))
     $msg = $_POST['msg'];
     if(empty($subject) || empty($email) || empty($msg))
     {
-        header('location:index.html?error');
+        header('location:index.php?error');
     }
     else 
     {
         $to="do4anand@gmail.com";
-        $headers =  'MIME-Version: 1.0' . "\r\n"; 
-        $headers .= "From: $email" . "\r\n";
-        $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n"; 
-        
-        if(mail($to,$subject,$msg,$headers))
+
+        if(mail($to,$subject,$msg,$email))
         {
             header('location:index.php?success');
         }
