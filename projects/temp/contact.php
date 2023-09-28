69d12c5989c361874e522172b202023729284f38
anand <do4connect@gmail.com>
2023-09-28 13:27:24 +0530

    temp data

---
+++ projects/temp/contact.php
@@ -0,0 +1,21 @@
+<?php
+// Get data from form 
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+ 
+$to = "anand@gmail.com";
+$subject = "This is the subject line";
+ 
+
+$txt ="Name = ". $name . "\r\n  Email = "
+    . $email . "\r\n Message =" . $message;
+ 
+$headers = "From: do4anand@gmail.com" . "\r\n";
+if($email != NULL) {
+    mail($to, $subject, $txt, $headers);
+}
+ 
+// Redirect to
+header("Location:index.html");
+?>
